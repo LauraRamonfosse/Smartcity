77971c00a33e22530056f76175f1c34437442b68
de Sousa Barbosa Diego <81906437+DeSousaDiego@users.noreply.github.com>
2023-11-18 17:27:52 +0100

    Update index.js

--- API/route/index.js
+++ API/route/index.js
@@ -1,12 +1,6 @@
-const ProduitRouter = require('./produit');
-const AchatRouter = require('./achat');
-const ClientRouter = require('./client');
 const UserRouter = require('./user');
 const router = require("express").Router();
 
-router.use("/produit", ProduitRouter);
-router.use("/achat", AchatRouter);
-router.use("/client", ClientRouter);
 router.use("/users", UserRouter);
 
 module.exports = router;
