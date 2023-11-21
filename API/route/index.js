2814e3a4273a2157fb72b1bdd0b90b8133375b52
Toon <45061689+Antjul@users.noreply.github.com>
2023-11-21 17:47:47 +0100

    Update index.js
    ajout de la route /comment

--- API/route/index.js
+++ API/route/index.js
@@ -1,7 +1,9 @@
 const UserRouter = require('./user');
+const CommentRouter = require('./comment');
 const router = require("express").Router();
 
 router.use("/users", UserRouter);
+router.use("/comment", CommentRouter);
 
 module.exports = router;
 
