677feea2e99ab7dc1a98ad2d7bc276654200ed13
Toon <45061689+Antjul@users.noreply.github.com>
2023-11-22 16:58:11 +0100

    Update index.js

--- API/route/index.js
+++ API/route/index.js
@@ -2,12 +2,14 @@ const UserRouter = require('./user');
 const CommentRouter = require('./comment');
 const BookRouter = require('./book');
 const RoleRouter = require('./role');
+const ReviewRouter = require('./review');
 const router = require("express").Router();
 
 router.use("/users", UserRouter);
 router.use("/comment", CommentRouter);
 router.use('/book', BookRouter);
 router.use('/role', RoleRouter);
+router.use("/review", ReviewRouter);
 
 module.exports = router;
 
