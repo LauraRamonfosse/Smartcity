ca934f50e05cca2d8d48563f3936780c16114964
anasarsalane <71637311+anasarsalane@users.noreply.github.com>
2023-11-22 11:04:51 +0100

    Update index.js

--- API/route/index.js
+++ API/route/index.js
@@ -1,9 +1,13 @@
 const UserRouter = require('./user');
 const CommentRouter = require('./comment');
+const BookRouter = require('./book');
+const RoleRouter = require('./role');
 const router = require("express").Router();
 
 router.use("/users", UserRouter);
 router.use("/comment", CommentRouter);
+router.use('/book', BookRouter);
+router.use('/role', RoleRouter);
 
 module.exports = router;
 
