e2ca9bb36d14bbbcc5fe86c07341a82339f9d3f9
anasarsalane <71637311+anasarsalane@users.noreply.github.com>
2023-12-04 17:03:03 +0100

    Update dataBaseLoader.js

--- BackOffice/src/store/dataBaseLoader.js
+++ BackOffice/src/store/dataBaseLoader.js
@@ -3,7 +3,8 @@ import { getAllBooks } from '../API/book';
 import { getAllReviews } from '../API/reviews';
 import { getAllRoles } from '../API/role';
 import { getAllActors } from '../API/actor';
-import { setBooks } from './slice/bookSlice';
+import { setBooks } from './bookSlice';
+import { setBestBooks } from './slice/bestBooksSlice';
 import { setReviews } from './slice/reviewSlice';
 import { setUsers } from './slice/userSlice';
 import { setRoles } from './slice/roleSlice';
@@ -19,13 +20,16 @@ const fetchBookData = async(dispatch, token) =>{
             bookDataRows.push([
                 {type: 'text', content: book.isbn},
                 {type: 'text', content: book.title},
+                {type: 'text', content: book.rating},
+                {type: 'text', content: book.author},
+                {type: 'text', content: book.illustrator? book.illustrator : "none"},
                 {type: 'text', content: book.description},
                 {type: 'text', content: book.country},
                 {type: 'text', content: book.genre},
                 {type: 'text', content: book.released_year},
                 {type: 'text', content: book.pages},
                 {type: 'text', content: book.publishing_house},
-                {type: 'text', content: book.img_path},
+                {type: 'text', content: book.img_path? book.img_path : "none"},
                 {type: 'modifyButton', content: 'Modify'},
                 {type: 'deleteButton', content: 'Delete'}
             ]);
@@ -37,6 +41,32 @@ const fetchBookData = async(dispatch, token) =>{
         console.error("Error fetching data: ", error);
     }
 };
+
+const fetchTopRatedBooks = async (dispatch, token) => {
+    try {
+        let bestDataRows = [];
+        let books = await getAllBooks(token);
+
+        // Trier les livres par ordre décroissant de rating
+        books.sort((a, b) => b.rating - a.rating);
+
+        // Prendre les 10 meilleurs livres
+        const top10Books = books.slice(0, 10);
+
+        top10Books.forEach((book) => {
+            bestDataRows.push([
+                { type: 'text', content : book.isbn },
+                { type: 'text', content: book.title },
+                { type: 'text', content: book.rating },
+            ]);
+        });
+        dispatch(setBestBooks(bestDataRows));
+    }
+    
+    catch (error) {
+        console.error("Error fetching data: ", error);
+    }
+}
 
 const fetchUserData = async (dispatch, token) => {
     try {
@@ -120,13 +150,11 @@ const fetchRoleData = async(dispatch, token) =>{
         const roles = await getAllRoles(token);
         roles.forEach(role => {
             roleDataRows.push([
-                {type: 'text', content: role.id_book},
-                {type: 'text', content: role.id_actor},
-                {type: 'text', content: role.title},
-                {type: 'modifyButton', content: 'Modify'},
-                {type: 'deleteButton', content: 'Delete'}
+                {type: 'text', content: role.id},
+                {type: 'text', content: role.role_name},
+                {type: 'text', content: role.actor_name},
+                {type: 'text', content: role.book_title},
             ]);
-
         });
         dispatch(setRoles(roleDataRows));
         
@@ -135,18 +163,41 @@ const fetchRoleData = async(dispatch, token) =>{
     }
 };
 
-const fetchActorData = async(dispatch, token) =>{
+const groupBooksByActor = async (token) => {
+    const actors = await getAllActors(token);
+    const actorMap = new Map();
+    console.log("GROUP BOOKS BY ACTOR");
+    actors.forEach(actor => {
+        const { id, actor_name, book_name } = actor;
+
+        if (!actorMap.has(actor_name)) {
+            actorMap.set(actor_name, { id, actor_name, books: [] });
+        }
+
+        if (book_name) {
+            actorMap.get(actor_name).books.push(book_name);
+        }
+    });
+
+    return Array.from(actorMap.values()).map(actor => ({
+        id: actor.id,
+        actor_name: actor.actor_name,
+        books: actor.books.join(', ') || "No association to a book"
+    }));
+};
+
+const fetchActorData = async(dispatch,token) =>{
     try {
         const actorDataRows = [];
-        const actors = await getAllActors(token);
-        actors.forEach(actor => {
-            actorDataRows.push([
-                {type: 'text', content: actor.id},
-                {type: 'text', content: actor.name},
-                {type: 'modifyButton', content: 'Modify'},
-                {type: 'deleteButton', content: 'Delete'}
-            ]);
+        console.log("HERE ARE THE ACTORS");
+        const actorData = await groupBooksByActor(token);
 
+        actorData.forEach(actor => {
+            actorDataRows.push([
+                {type: 'text', content : actor.id},
+                { type: 'text', content: actor.actor_name },
+                { type: 'text', content: actor.books },
+            ]);
         });
         dispatch(setActors(actorDataRows));
         
@@ -159,6 +210,7 @@ const fetchActorData = async(dispatch, token) =>{
 export const loadDataBase = async (dispatch, token) => {
     try {
         await fetchBookData(dispatch, token);
+        await fetchTopRatedBooks(dispatch,token);
         await fetchUserData(dispatch, token);
         await fetchReviewData(dispatch, token);
         // await fetchCommentData(dispatch, token);
