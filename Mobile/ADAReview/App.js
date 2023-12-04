fe1b60c0f3c13f0fd390a5db18c1da7c1c793835
anasarsalane <71637311+anasarsalane@users.noreply.github.com>
2023-12-04 20:03:23 +0100

    Add files via upload

---
+++ Mobile/ADAReview/App.js
@@ -0,0 +1,9 @@
+import MenuBar from "./src/components/MenuBar";
+
+function App(){
+  return (
+    <MenuBar/>
+  );
+}
+
+export default MenuBar;
---
+++ Mobile/ADAReview/src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+
+function Home (){
+    return(
+        <View style = {{flex: 1, alignItems : 'center', justifyContent : 'center'}}>
+            <Text>
+                HOME SCREEN
+            </Text>
+        </View>
+    )
+}
+export default Home;
