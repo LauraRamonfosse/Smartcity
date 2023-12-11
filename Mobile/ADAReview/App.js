884ff715b6f168fe8d1ea4913fcfc6c18cad89a9
anasarsalane <71637311+anasarsalane@users.noreply.github.com>
2023-12-11 23:11:22 +0100

    Delete Mobile/ADAReview directory

--- Mobile/ADAReview/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Navigation from './src/Navigation/Navigation'; // Importez le composant Navigation
-
-const App = () => {
-  return <Navigation />;
-};
-export default App;
